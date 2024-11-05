dd0ca1c0781feec5652457bc390f52d7d942f1bf
Brian Faust <brian@faust.de.com>
2024-11-05 07:36:32 +0200

    first commit
    Signed-off-by: Brian Faust <brian@faust.de.com>

---
+++ tests/Unit/FormatPercentageTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use BaseCodeOy\Formatter\FormatPercentage;
+
+it('formats the input value as a percentage', function (): void {
+    expect(FormatPercentage::execute(0))->toBe('0.00%');
+    expect(FormatPercentage::execute(50))->toBe('50.00%');
+    expect(FormatPercentage::execute(100))->toBe('100.00%');
+    expect(FormatPercentage::execute(5.6789))->toBe('5.68%');
+    expect(FormatPercentage::execute(-5.6789))->toBe('-5.68%');
+});
